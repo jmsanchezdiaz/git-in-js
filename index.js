32e225f8d04c3b8b12d61aec8e4544b52261f45f
jmsanchezdiaz <jmsanchezdiaz02@gmail.com>
2025-03-07 20:47:42 -0300

    branch initial logic

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ function generateUUID() {
   });
 }
 
+class Branch {
+  constructor(name, commit) {
+    this.name = name;
+    this.commit = commit;
+  }
+}
+
 class Commit {
   constructor(id, message, prevCommit) {
     this.id = id;
@@ -22,19 +29,39 @@ class Commit {
 class Git {
   constructor(name) {
     this.name = name;
-    this.HEAD = null;
+    this.HEAD = new Branch("master", null);
+    this.branches = [this.HEAD]; // Could be a dictionary to be more performant
+  }
+  
+
+  checkout(branchName) {
+    // Looking for the branch.
+    for(let branch of this.branches) {
+      if(branch.name === branchName) {
+        console.log("SWITCHING to " + branchName + " from " + this.HEAD.name)
+        this.HEAD = branch
+        return branch
+      }
+    }
+    
+    // Creating if not found
+    const newBranch = new Branch(branchName, this.HEAD.commit);
+    this.branches.push(newBranch);
+    console.log("SWITCHING to " + branchName + " from " + this.HEAD.name)
+    this.HEAD = newBranch;
+    return newBranch;
   }
   
   commit(message){
-    const newCommit = new Commit(generateUUID(), message, this.HEAD)
-    this.HEAD = newCommit;
+    const newCommit = new Commit(generateUUID(), message, this.HEAD.commit)
+    this.HEAD.commit = newCommit;
     return newCommit;
   }
   
   log(){
     const history = []
     
-    let start = this.HEAD
+    let start = this.HEAD.commit
     
     while(start){
       history.push(start)
@@ -51,4 +78,10 @@ repo.commit("hotfix") // git commit -m "hotfix"
 repo.commit("correcting") // git commit -m "hotfix"
 repo.commit("moneyyyy") // git commit -m "hotfix"
 
-console.log(repo.log())
+// console.log(repo.log())
+
+// repo.checkout("testing")
+
+// repo.commit("Mondongo")
+
+// console.log(repo.log())
